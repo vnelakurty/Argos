9ba70c8f45ecbd5a374916372c58c3c57c801658
NELAKURTY <vinay.a.nelakurty@capgemini.com>
2022-01-25 10:50:54 +0000

    new commit

---
+++ src/main/java/Argos/Argos/App.java
@@ -0,0 +1,13 @@
+package Argos.Argos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
